521738ffd476b1a7e379cda62f98658cff35e11e
mahendirans <88781391+mahendirans@users.noreply.github.com>
2021-09-27 18:07:11 +0530

    Add files via upload

---
+++ ATMCaseStudy.java
@@ -0,0 +1,12 @@
+package test;
+
+public class ATMCaseStudy {
+	 // main method creates and runs the ATM
+	   public static void main(String[] args)
+	   {
+		  
+	      ATM theATM = ATM.getinstance();
+	      
+	      theATM.run();
+	   } // end main
+	} // end class ATMCaseStudy 
